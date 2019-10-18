f27b1967b51aa2e725dafd3cfb77d11e43b7f482
Vignesh Natarajan <vignesh.natarajan@syncfusion.com>
2019-10-18 16:38:09 +0530

    sample

---
+++ WebAPI/Data/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Data;
+
+namespace WebAPI.Data
+{
+    public class OrderService
+    {
+        string baseUrl = "http://localhost:50838/";
+        public async Task<List<Orders>> GetOrdersAsync()
+        {
+            HttpClient http = new HttpClient();
+            var json = await http.GetStringAsync($"{baseUrl}api/Default");
+            return JsonConvert.DeserializeObject<List<Orders>>(json);
+        }
+        public async Task<HttpResponseMessage> InsertOrderAsync(Orders value)
+        {
+            var client = new HttpClient();
+            return await client.PostAsync($"{baseUrl}api/Default", getStringContentFromObject(value));
+        }
+        public async Task<HttpResponseMessage> UpdateOrderAsync(string id,Orders value)
+        {
+            var client = new HttpClient();
+            return await client.PutAsync($"{baseUrl}api/Default/{id}", getStringContentFromObject(value));            
+        }
+
+        public async Task<HttpResponseMessage> DeleteOrderAsync(string id)
+        {
+            var client = new HttpClient();
+            return await client.DeleteAsync($"{baseUrl}api/Default/{id}");            
+        }
+        private StringContent getStringContentFromObject(object obj)
+        {
+            var serialized = JsonConvert.SerializeObject(obj);
+            var stringContent = new StringContent(serialized, Encoding.UTF8, "application/json");
+            return stringContent;
+        }
+    }
+}
---
+++ WebAPI/Data/Orders.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Data
+{
+    public class Orders
+    {
+        public Orders()
+        {
+
+        }
+        public Orders(long OrderId, string CustomerId, Nullable<int> EmployeeId, double Freight, DateTime OrderDate, string ShipCity)
+        {
+            this.OrderID = OrderId;
+            this.CustomerID = CustomerId;
+            this.EmployeeID = EmployeeId;
+            this.Freight = Freight;
+            //this.Time = Time;
+            this.OrderDate = OrderDate;
+            //this.HireDate = HireDate;
+           // this.HireDate1 = HireDate1;
+            this.ShipCity = ShipCity;
+        }
+        public long OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public Nullable<int> EmployeeID { get; set; }
+
+      //  public TimeSpan? Time { get; set; }
+        public double Freight { get; set; }
+        public DateTime OrderDate { get; set; }
+        //public DateTimeOffset? HireDate { get; set; }
+        //public DateTimeKind? HireDate1 { get; set; }
+        public string ShipCity { get; set; }
+    }
+}
