20f75b5fc86be5501ba3a4a946e0f28c4801ffe8
BackiaRaj <backiaraj.kanagaraj@syncfusion.com>
2023-12-29 11:14:37 +0530

    Update the project

---
+++ Data/Orders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudAsynchronousSample.Data
+{
+    public class Orders
+    {
+        public Orders()
+        {
+
+        }
+        public Orders(long OrderId, string CustomerId, Nullable<int> EmployeeId, double Freight, DateTime OrderDate, string ShipCity)
+        {
+            this.OrderID = OrderId;
+            this.CustomerID = CustomerId;
+            this.EmployeeID = EmployeeId;
+            this.Freight = Freight;
+            this.OrderDate = OrderDate;
+            this.ShipCity = ShipCity;
+        }
+        public long OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public Nullable<int> EmployeeID { get; set; }
+        public double Freight { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? ShipCity { get; set; }
+    }
+}
